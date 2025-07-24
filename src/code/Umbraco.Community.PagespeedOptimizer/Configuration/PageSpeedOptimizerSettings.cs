cecbb99ffa830f58b8a1dd12c3083fa7db14b569
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2025-07-24 14:56:15 +0200

    Added objec for configuration of package in app settings

---
+++ src/code/Umbraco.Community.PagespeedOptimizer/Configuration/PageSpeedOptimizerSettings.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Umbraco.Community.PagespeedOptimizer.Configuration;
+
+/// <summary>
+/// Settings object for page speed optimizer.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public sealed class PageSpeedOptimizerSettings
+{
+    /// <summary>
+    /// The configuration section name.
+    /// </summary>
+    public const string SectionName = "Umbraco:Community:PageSpeedOptimizer";
+}
