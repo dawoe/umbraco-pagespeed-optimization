e530700ecfd51a5e4f564daae5e5abeeac20ceb1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2025-12-08 10:45:41 +0100

    Added v17 test website

--- test-sites/Website-V17/Program.cs
+++ test-sites/Website-V17/Program.cs
@@ -5,7 +5,6 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
-    .AddDeliveryApi()
     .AddComposers()
     .Build();
 
@@ -16,16 +15,15 @@ await app.BootUmbracoAsync();
 
 
 app.UseUmbraco()
-    .WithMiddleware(u =>
-    {
-        u.UseBackOffice();
-        u.UseWebsite();
-    })
-    .WithEndpoints(u =>
-    {
-        u.UseInstallerEndpoints();
-        u.UseBackOfficeEndpoints();
-        u.UseWebsiteEndpoints();
-    });
+	.WithMiddleware(u =>
+	{
+		u.UseBackOffice();
+		u.UseWebsite();
+	})
+	.WithEndpoints(u =>
+	{
+		u.UseBackOfficeEndpoints();
+		u.UseWebsiteEndpoints();
+	});
 
 await app.RunAsync();
