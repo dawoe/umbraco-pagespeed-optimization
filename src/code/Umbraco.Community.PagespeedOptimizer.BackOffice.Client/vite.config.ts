0fdf301757899596079e0121a5fdd5df0537c7aa
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2026-07-01 10:52:10 +0200

    Setup build to use correct output folder

--- src/code/Umbraco.Community.PagespeedOptimizer.BackOffice.Client/vite.config.ts
+++ src/code/Umbraco.Community.PagespeedOptimizer.BackOffice.Client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
             formats: ["es"],
             fileName: "entry-point", // the name of the built file
         },
-        outDir: "../Umbraco.Community.PagespeedOptimizer.BackOffice.Client/wwwroot/App_Plugins/pagespeedoptimizer", // all compiled files will be placed here
+        outDir: "../Umbraco.Community.PagespeedOptimizer.BackOffice/wwwroot/App_Plugins/pagespeedoptimizer", // all compiled files will be placed here
         emptyOutDir: true,
         sourcemap: true,
         rollupOptions: {
